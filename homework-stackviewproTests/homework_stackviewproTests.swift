015679d34e30bc00a380e0fbf1987f7335487f0d
Ricky <ricky.hero@gmail.com>
2020-11-19 16:32:52 +0800

    Initial Commit

---
+++ homework-stackviewproTests/homework_stackviewproTests.swift
@@ -0,0 +1,33 @@
+//
+//  homework_stackviewproTests.swift
+//  homework-stackviewproTests
+//
+//  Created by Ricky on 2020/11/16.
+//
+
+import XCTest
+@testable import homework_stackviewpro
+
+class homework_stackviewproTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
